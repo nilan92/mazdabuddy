218ea6ab6a7e9965d9e2506b4e869b1f1c3ab894
Nilan Iddawela <nilan92@gmail.com>
2026-09-24 19:58:07 +0530

    Updates

---
+++ assets/arrow-left-D5h8HVDL.js
@@ -0,0 +1,2 @@
+import{c as o}from"./index-DJ-VSiU9.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
+//# sourceMappingURL=arrow-left-D5h8HVDL.js.map
